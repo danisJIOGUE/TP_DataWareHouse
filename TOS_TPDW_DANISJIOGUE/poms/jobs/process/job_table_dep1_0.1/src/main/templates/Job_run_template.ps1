794cbc9606fb0f4b7f3558ebfe8031f5cbf825c6
DanisJIOGUE <danisjiogue@gmail.com>
2020-06-30 19:26:53 +0000

    TP Data warehouse

---
+++ TOS_TPDW_DANISJIOGUE/poms/jobs/process/job_table_dep1_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/dom4j-1.6.1.jar;../lib/jtds-1.3.1-patch.jar;../lib/log4j-1.2.17.jar;../lib/mysql-connector-java-8.0.12.jar;job_table_dep1_0_1.jar;' tpdw_danisjiogue.job_table_dep1_0_1.job_table_dep1  %*
